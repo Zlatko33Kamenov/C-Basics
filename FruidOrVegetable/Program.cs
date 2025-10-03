4542f784ee5d2b8d60c42365d5bab5bb558ee33a
Zlatko33Kamenov <138592118+Zlatko33Kamenov@users.noreply.github.com>
2025-10-03 17:54:41 +0300

    Exercise #3

---
+++ FruidOrVegetable/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Zad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            switch (product)
+            {
+                case "banana":
+                    Console.WriteLine("fruid");
+                    break;
+                case "apple":
+                    Console.WriteLine("fruid");
+                    break;
+                case "kiwi":
+                    Console.WriteLine("fruid");
+                    break;
+                case "cherry":
+                    Console.WriteLine("fruid");
+                    break;
+                case "lemon":
+                    Console.WriteLine("fruid");
+                    break;
+                case "grapes":
+                    Console.WriteLine("fruid");
+                    break;
+                case "tomato":
+                    Console.WriteLine("vegetable");
+                    break;
+                case "cucumber":
+                    Console.WriteLine("vegetable");
+                    break;
+                case "pepper":
+                    Console.WriteLine("vegetable");
+                    break;
+                case "carrot":
+                    Console.WriteLine("vegetable");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}

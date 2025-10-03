203c84ca5450311c9dd62be031e9fc14b40b2e4a
Zlatko33Kamenov <138592118+Zlatko33Kamenov@users.noreply.github.com>
2025-10-03 17:53:38 +0300

    Exercise #2

---
+++ TicketsMachine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zad9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ticketType = Console.ReadLine();
+            byte rows = byte.Parse(Console.ReadLine());
+            byte cows = byte.Parse(Console.ReadLine());
+            int ticketsAmount = rows * cows;
+            switch (ticketType)
+            {
+                case "Normal":
+                    Console.WriteLine($"{ticketsAmount*7.50:F2}");
+                    break;
+                case "Premiere":
+                    Console.WriteLine($"{ticketsAmount * 12.00:F2}");
+                    break;
+                case "Discount":
+                    Console.WriteLine($"{ticketsAmount * 5.00:F2}");
+                    break;
+                
+            }
+
+        }
+    }
+}
